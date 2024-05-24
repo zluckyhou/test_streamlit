f395afb69abd0a2580d00d56d5d8f6882189b5b4
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-24 10:12:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ message = st.chat_input("发送消息")
 
 if message:
     # 显示聊天消息
-    st.chat_message("user").write(message)
-
-    # 如果有上传的图片，并且这是第一次发送消息，显示图片
-    if st.session_state.uploaded_image is not None:
-        st.chat_message("user").image(st.session_state.uploaded_image)
-
-        # 发送图片后，将上传的图片从 session state 中移除
-        st.session_state.uploaded_image = None
+    with st.chat_message("user"):
+        st.write(message)
+        # 如果有上传的图片，并且这是第一次发送消息，显示图片
+        if st.session_state.uploaded_image is not None:
+            st.image(st.session_state.uploaded_image)
+    
+    # 发送图片后，将上传的图片从 session state 中移除
+    st.session_state.uploaded_image = None
 
     # 显示回复消息
     st.chat_message("assistant").write("这是你的回复消息。")
