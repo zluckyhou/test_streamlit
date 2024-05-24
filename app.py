b11a2ac520b122de29b637af7196e42b8b388916
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-24 11:22:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -76,19 +76,10 @@
 #         st.session_state['uploaded_image'] = None
 
 import streamlit as st
+import time
 
-placeholder = st.empty()
-
-# Replace the placeholder with some text:
-placeholder.text("Hello")
-
-# Replace the text with a chart:
-placeholder.line_chart({"data": [1, 5, 2, 6]})
-
-# Replace the chart with several elements:
-with placeholder.container():
-    st.write("This is one element")
-    st.write("This is another")
-
-# Clear all those elements:
-placeholder.empty()
+with st.empty():
+    for seconds in range(3):
+        st.write(f"⏳ {seconds} seconds have passed")
+        time.sleep(1)
+    st.write("✔️ 1 minute over!")
