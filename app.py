809f9d1748c8419a826fc0fdecf8e2eef5d8fbe4
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-28 16:46:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ with st.sidebar:
 
 st.title("This is title")
 
+st.markdown("""
+<script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
+<script>
+  kofiWidgetOverlay.draw('chatgpt4o', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#fcbf47',
+    'floating-chat.donateButton.text-color': '#323842'
+  });
+</script>
+"""
+)
+
 
 prompt = st.chat_input("What's up?")
 
