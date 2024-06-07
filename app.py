0e0087285f83a314be1bf2929f9689cf28fc797e
zluckyH <zluckyhou@users.noreply.github.com>
2024-06-07 12:59:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,4 @@ import os
 
 wav_audio_data = st_audiorec()
 
-if wav_audio_data is not None:
-	st.markdown(f"wav_audio_data: {wav_audio_data}")
-
-	st.audio(wav_audio_data, format='audio/wav')
+st.markdown(f"wav_audio_data: {wav_audio_data}")
